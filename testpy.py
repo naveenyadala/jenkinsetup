b67ed4b69657c35a8969a2d4725950ec1d2f0065
NaveenUbuntu <you@example.com>
2018-11-02 20:47:36 +0530

    Python code

---
+++ testpy.py
@@ -0,0 +1,10 @@
+class Test:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+    def __str__(self):
+        return self.first + self.last 
+
+if __name__ == '__main__':
+    de = Test('naveen', 'prashanth')
+    print(de)
